db24885d0ce5b24e9bfc3ea9cbcee18c41c99f98
Zircon <zircon@example.com>
2026-07-13 21:01:17 +0530

    Update core server logic

--- app/main.js
+++ app/main.js
@@ -6,6 +6,9 @@ console.log("Logs from your program will appear here!");
 // Uncomment the code below to pass the first stage
 const server = net.createServer((connection) => {
   // Handle connection
+  connection.on("data", (data) => {
+    connection.write("+PONG\r\n");
+  });
 });
 
 server.listen(6379, "127.0.0.1");
